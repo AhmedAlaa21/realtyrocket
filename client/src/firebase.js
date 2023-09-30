f525c39145eff2d542b3136a1a9b30a4d017d49f
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-10-01 02:12:16 +0300

    create and add OAuth Component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD99YnYXOxOTLY0YGGuDkBlBkJ5HzJlUus",
+  authDomain: "realtyrocket-75dda.firebaseapp.com",
+  projectId: "realtyrocket-75dda",
+  storageBucket: "realtyrocket-75dda.appspot.com",
+  messagingSenderId: "936856889256",
+  appId: "1:936856889256:web:0314e5f2d1df803ffa9f9d",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
