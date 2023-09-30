402df8241730fb143f52ee928cfd2a59e37ce7ce
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-09-30 07:59:43 +0300

    initialized db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL).then(()=> {
+    console.log('Connected to database')
+}).catch(err => console(err));
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log('Server is listening on port 3000!!!')
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000!!!");
 });
-
