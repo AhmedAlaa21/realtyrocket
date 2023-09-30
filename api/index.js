13a0b9ce203effbd6a8389475e074ede745ff86c
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2023-09-30 11:24:09 +0300

    complete signup page functionality

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -16,8 +17,9 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000!!!");
+app.use(cors());
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000!!!");
 });
 
 app.use("/api/user", userRouter);
@@ -27,5 +29,5 @@ app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({ success: false,statusCode, message });
+  return res.status(statusCode).json({ success: false, statusCode, message });
 });
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,78 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.error);
+        setLoading(false);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in')
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Sign up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
           className="border p-3 rounded-lg focus:outline-none"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="E-mail"
           className="border p-3 rounded-lg focus:outline-none"
           id="email"
+          onChange={handleChange}
         />
         <input
-          type="passowrd"
+          type="password"
           placeholder="Password"
           className="border p-3 rounded-lg focus:outline-none"
           id="password"
+          onChange={handleChange}
         />
         <button
           className="bg-slate-700 p-3 text-white rounded-lg uppercase
         hover:opacity-95 disabled:opacity-80"
+          type="submit"
+          disabled={loading}
         >
-          SignUp
+          {loading ? "Loading..." : "SignUp"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -36,6 +81,7 @@ export default function SignUp() {
           <span className="text-blue-700 font-semibold underline">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
